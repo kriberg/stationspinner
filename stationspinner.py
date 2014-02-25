2602a4d26ea703b18451541d1826d64eaba6dcb9
Kristian Berg <kriberg@tihlde.org>
2014-02-25 23:20:15 +0100

    Fix a database connect issue, fixed sorting

--- stationspinner.py
+++ stationspinner.py
@@ -10,73 +10,59 @@ config = ConfigParser.SafeConfigParser()
 config.readfp(open('stationspinner.ini'))
 db_params = config.items('database')
 conn_string = ' '.join(map(lambda a: '%s=%s' % (a[0], a[1]), db_params))
-conn = psycopg2.connect(conn_string)
+
 
 @app.route('/stationspinner/types/')
 def invtypes():
     rv = cache.get('types')
     if rv is None:
-        with conn.cursor() as cur:
-            cur.execute('SELECT array_to_json(array_agg(row_to_json(t))) FROM \
-                    (SELECT typeid, typename, marketgroupid FROM \
-                    public.invtypes WHERE published=1 AND \
-                    volume > 0.0 limit 10) t')
-            rv = cur.fetchone()[0]
-            cache.set('types', rv)
+        with psycopg2.connect(conn_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute('SELECT array_to_json(array_agg(row_to_json(t))) FROM \
+                        (SELECT typeid, typename, marketgroupid FROM \
+                        public.invtypes WHERE published=1 AND \
+                        volume > 0.0) t')
+                rv = cur.fetchone()[0]
+                cache.set('types', rv)
     return '%s' % rv
 
 @app.route('/stationspinner/marketitems/')
 def marketitems():
     rv = cache.get('marketitems')
     if rv is None:
-        with conn.cursor() as cur:
-            cur.execute('''SELECT array_to_json(array_agg(row_to_json(t))) FROM
-                    (SELECT invtypes.typename,
-                      mapdenormalize.itemname,
-                      (sell_min-buy_max) AS "Profit",
-                      CASE WHEN sell_min <> 0 THEN ROUND(100-(buy_max/sell_min*100), 2) ELSE 0 END AS "Profit margin",
-                      marketdata."timestamp",
-                      marketdata.sell_percentile,
-                      marketdata.sell_min,
-                      marketdata.buy_percentile,
-                      marketdata.buy_max,
-                      invmarketgroups.marketgroupname,
-                      invcategories.categoryname
-                    FROM
-                      public.invmarketgroups,
-                      public.invcategories,
-                      public.invtypes,
-                      public.marketdata,
-                      public.mapdenormalize,
-                      public.invgroups
-                    WHERE
-                      invmarketgroups.marketgroupid = invtypes.marketgroupid AND
-                      invcategories.categoryid = invgroups.categoryid AND
-                      marketdata.typeid = invtypes.typeid AND
-                      mapdenormalize.itemid = marketdata.locationid AND
-                      invgroups.groupid = invtypes.groupid
-                    ORDER BY
-                      invtypes.typename ASC,
-                      mapdenormalize.itemname ASC,
-                      invmarketgroups.marketgroupname ASC,
-                      invcategories.categoryname ASC,
-                      marketdata.buy_volume ASC,
-                      marketdata.buy_avg ASC,
-                      marketdata.buy_max ASC,
-                      marketdata.buy_min ASC,
-                      marketdata.buy_stddev ASC,
-                      marketdata.buy_median ASC,
-                      marketdata.buy_percentile ASC,
-                      marketdata.sell_volume ASC,
-                      marketdata.sell_avg ASC,
-                      marketdata.sell_max ASC,
-                      marketdata.sell_min ASC,
-                      marketdata.sell_stddev ASC,
-                      marketdata.sell_median ASC,
-                      marketdata.sell_percentile ASC,
-                      marketdata."timestamp" ASC) t''')
-            rv = cur.fetchone()[0]
-            cache.set('marketitems', rv, timeout=15*60)
+        with psycopg2.connect(conn_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute('''SELECT array_to_json(array_agg(row_to_json(t))) FROM
+                        (SELECT invtypes.typename,
+                          mapdenormalize.itemname,
+                          (sell_min-buy_max) AS "Profit",
+                          CASE WHEN sell_min <> 0 THEN ROUND(100-(buy_max/sell_min*100), 2) ELSE 0 END AS "Profit margin",
+                          marketdata."timestamp",
+                          marketdata.sell_percentile,
+                          marketdata.sell_min,
+                          marketdata.buy_percentile,
+                          marketdata.buy_max,
+                          invmarketgroups.marketgroupname,
+                          invcategories.categoryname
+                        FROM
+                          public.invmarketgroups,
+                          public.invcategories,
+                          public.invtypes,
+                          public.marketdata,
+                          public.mapdenormalize,
+                          public.invgroups
+                        WHERE
+                          invmarketgroups.marketgroupid = invtypes.marketgroupid AND
+                          invcategories.categoryid = invgroups.categoryid AND
+                          marketdata.typeid = invtypes.typeid AND
+                          mapdenormalize.itemid = marketdata.locationid AND
+                          invgroups.groupid = invtypes.groupid
+                        ORDER BY
+                          "Profit margin" DESC,
+                          invtypes.typename ASC,
+                          mapdenormalize.itemname ASC) t''')
+                rv = cur.fetchone()[0]
+                cache.set('marketitems', rv, timeout=15*60)
     return '%s' % rv
 
 if not app.debug:
