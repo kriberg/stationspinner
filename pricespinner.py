ba2e9dcd6f1292dd89e892c8233ded35a4ef2bf7
Kristian Berg <kriberg@tihlde.org>
2014-04-27 21:49:58 +0200

    Multithreaded eve-central parsing

--- pricespinner.py
+++ pricespinner.py
@@ -1,30 +1,39 @@
 import psycopg2
+from psycopg2.pool import ThreadedConnectionPool
+from threading import Thread
 import logging
 import sys
 import ConfigParser
-import httplib
+import urllib2
 import json
+import Queue
 from evelink.thirdparty.eve_central import EVECentral
 from traceback import format_exc
 
 MARKET_ITEMS = 'SELECT typeid FROM invtypes WHERE published=1 AND marketgroupid IS NOT NULL AND marketgroupid != 350001'
+CREST_URL = 'http://public-crest.eveonline.com/market/%s/types/%s/history/'
 
 class PriceSpinner():
     BLOCK_SIZE = 100
+    hqueue = Queue.Queue()
+    dqueue = Queue.Queue()
     def __init__(self, configfile):
         self.config = ConfigParser.SafeConfigParser()
         self.config.readfp(open(configfile))
         self.log = logging.getLogger('pricespinner')
         handler = logging.FileHandler(self.config.get('pricespinner', 'log'))
-        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        formatter = logging.Formatter('%(asctime)s %(thread)s %(levelname)s %(message)s')
         handler.setFormatter(formatter)
         self.log.addHandler(handler)
         self.log.setLevel(logging.DEBUG)
+        self.hthreads = int(self.config.get('pricespinner', 'history_threads'))
+        self.dthreads = int(self.config.get('pricespinner', 'data_threads'))
 
         db_params = self.config.items('database')
         conn_string = ' '.join(map(lambda a: '%s=%s' % (a[0], a[1]), db_params))
         self.conn = psycopg2.connect(conn_string)
         self.conn.autocommit = True
+        self.pool = ThreadedConnectionPool(self.hthreads+self.dthreads, 100, conn_string)
         self.ec = EVECentral()
     def cursor(self):
         return self.conn.cursor()
@@ -43,60 +52,135 @@ class PriceSpinner():
         res = cur.fetchall()
         return res
 
-    def pull_prices(self):
+    def get_items(self):
+        cur = self.cursor()
+        cur.execute(MARKET_ITEMS)
+        res = cur.fetchall()
+        typeids = map(lambda t: t[0], res)
+        return typeids
+
+    def fill_crest_queue(self):
+        url = 'http://public-crest.eveonline.com/market/%s/types/%s/history/'
+        regions = self.get_region_ids()
+        items = self.get_items()
+
+        for regionid, regionname in regions:
+            for typeid in items:
+                self.hqueue.put((regionid, typeid))
+
+    def fill_evecentral_queue(self):
         with self.cursor() as cur:
             cur.execute(MARKET_ITEMS)
             systems = self.get_system_ids()
-            # regions = self.get_region_ids()
 
             block = cur.fetchmany(size=self.BLOCK_SIZE)
             while block:
                 typeids = map(lambda t: t[0], block)
                 for system in systems:
-                    try:
-                        yield system, self.ec.market_stats(typeids, system=system[0])
-                    except Exception, ex:
-                        self.log.error('System: %s Error:\n%sTypes:\n%s' % (system[1], format_exc(ex), typeids))
-                # for region in regions:
-                #     try:
-                #         yield region, self.ec.market_stats(typeids, regions=region[0])
-                #     except Exception, ex:
-                #         self.log.error('Region: %s Error:\n%sTypes:\n%s' % (region[1], format_exc(ex), typeids))
+                    self.dqueue.put((system, typeids))
                 block = cur.fetchmany(size=self.BLOCK_SIZE)
-    def refresh_market_prices(self):
+
+    def refresh_crest_history(self):
         with self.cursor() as cur:
-            cur.execute('SELECT count(*) FROM invtypes WHERE published=1 AND marketgroupid is not null')
-            total_items = cur.fetchone()[0]
-        processed_items = 0
-        self.log.info('Fetching market prices for %s items' % total_items)
-        for location, prices in self.pull_prices():
-            # 22: {'all': {'avg': 2866.75,
-            #         'max': 5110.1,
-            #         'median': 3180.31,
-            #         'min': 1265.0,
-            #         'percentile': 1683.28,
-            #         'stddev': 705.11,
-            #         'volume': 818746},
-            #     'buy': {'avg': 2749.09,
-            #         'max': 3191.07,
-            #         'median': 3113.77,
-            #         'min': 1265.0,
-            #         'percentile': 3190.69,
-            #         'stddev': 722.74,
-            #         'volume': 689444},
-            #     'id': 22,
-            #     'sell': {'avg': 3494.12,
-            #         'max': 5110.1,
-            #         'median': 3472.87,
-            #         'min': 3379.9,
-            #         'percentile': 3390.52,
-            #         'stddev': 429.8,
-            #         'volume': 129302}},
-            with self.cursor() as cur:
+            cur.execute('SELECT MAX(timestamp) FROM markethistory')
+            res = cur.fetchone()[0]
+            print res
+        for i in range(self.hthreads):
+            t = Thread(target=self.crest_worker)
+            t.daemon = True
+            t.start()
+        self.fill_crest_queue()
+        self.log.info('Fetching %s crest market histories' % self.hqueue.qsize())
+        self.hqueue.join()
+
+    def refresh_evecentral_prices(self):
+        self.log.info('Fetching eve central prices')
+        for i in range(self.dthreads):
+            t = Thread(target=self.evecentral_worker)
+            t.daemon = True
+            t.start()
+        self.fill_evecentral_queue()
+        self.dqueue.join()
+
+    def crest_worker(self):
+        conn = self.pool.getconn()
+        conn.autocommit = True
+        with conn.cursor() as cur:
+            while True:
+                regionid, typeid = self.hqueue.get()
+                try:
+                    req = urllib2.urlopen(CREST_URL % (regionid, typeid))
+                    type_history = json.load(req)
+                    req.close()
+                except Exception, ex:
+                    self.log.error('Region: %s, Type: %s Error:\n%s' % (regionid, typeid, format_exc(ex)))
+                    continue
+                history_data = []
+                for marketdata in type_history['items']:
+                    marketdata['locationid'] = regionid
+                    marketdata['typeid'] = typeid
+                    marketdata['volume'] = int(marketdata['volume_str'])
+                    marketdata['orderCount'] = int(marketdata['orderCount_str'])
+                    marketdata.pop('volume_str')
+                    marketdata.pop('orderCount_str')
+                    history_data.append(marketdata)
+                cur.executemany('''
+                    WITH upsert AS (UPDATE markethistory SET
+                        timestamp=%(date)s,
+                        typeid=%(typeid)s,
+                        locationid=%(locationid)s,
+                        volume=%(volume)s,
+                        orders=%(orderCount)s,
+                        low=%(lowPrice)s,
+                        high=%(highPrice)s,
+                        avg=%(avgPrice)s
+                    WHERE typeid=%(typeid)s AND locationid = %(locationid)s AND timestamp=%(date)s RETURNING *)
+                    INSERT INTO markethistory (
+                        timestamp, typeid, locationid, volume, orders, low, high, avg)
+                    SELECT
+                        %(date)s, %(typeid)s, %(locationid)s, %(volume)s,
+                        %(orderCount)s, %(lowPrice)s, %(highPrice)s, %(avgPrice)s
+                    WHERE NOT EXISTS (SELECT * FROM upsert)''', tuple(history_data))
+                self.log.debug('Item %s, region %s done.' % (typeid, regionid))
+                self.hqueue.task_done()
+
+    def evecentral_worker(self):
+        conn = self.pool.getconn()
+        conn.autocommit = True
+        with conn.cursor() as cur:
+            while True:
+                system, typeids = self.dqueue.get()
+                try:
+                    prices = self.ec.market_stats(typeids, system=system[0])
+                except Exception, ex:
+                    self.log.error('System: %s Error:\n%sTypes:\n%s' % (system[1], format_exc(ex), typeids))
+                    continue
+                # 22: {'all': {'avg': 2866.75,
+                #         'max': 5110.1,
+                #         'median': 3180.31,
+                #         'min': 1265.0,
+                #         'percentile': 1683.28,
+                #         'stddev': 705.11,
+                #         'volume': 818746},
+                #     'buy': {'avg': 2749.09,
+                #         'max': 3191.07,
+                #         'median': 3113.77,
+                #         'min': 1265.0,
+                #         'percentile': 3190.69,
+                #         'stddev': 722.74,
+                #         'volume': 689444},
+                #     'id': 22,
+                #     'sell': {'avg': 3494.12,
+                #         'max': 5110.1,
+                #         'median': 3472.87,
+                #         'min': 3379.9,
+                #         'percentile': 3390.52,
+                #         'stddev': 429.8,
+                #         'volume': 129302}},
                 price_data = []
                 for typeid in prices.keys():
                     price = prices[typeid]
-                    type_data = {'typeid': typeid, 'locationid': location[0]}
+                    type_data = {'typeid': typeid, 'locationid': system[0]}
                     for order_type in ('buy', 'sell'):
                         for figure, value in price[order_type].items():
                             type_data['%s_%s' % (order_type, figure)] = value
@@ -131,11 +215,12 @@ class PriceSpinner():
                     %(sell_volume)s, %(sell_avg)s, %(sell_max)s, %(sell_min)s,
                     %(sell_stddev)s, %(sell_median)s, %(sell_percentile)s
                     WHERE NOT EXISTS (SELECT * FROM upsert)''', tuple(price_data))
-                processed_items += cur.rowcount
-                self.log.debug('Block done, %d items for location %s' % (len(price_data), location[1]))
-        self.log.info('Processed %s items in total' % processed_items)
+                self.log.debug('Block done, %d items for location %s' % (len(price_data), system[1]))
+                self.dqueue.task_done()
+
+        self.pool.putconn(conn)
 
 
 if __name__ == '__main__':
     ps = PriceSpinner('stationspinner.ini')
-    ps.refresh_market_prices()
+    ps.refresh_evecentral_prices()
