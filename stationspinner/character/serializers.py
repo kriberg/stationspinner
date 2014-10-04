ecdf7d684db4a97085847cf8038be9c9770d52df
Kristian Berg <kriberg@tihlde.org>
2014-10-04 21:12:31 +0200

    Add character sheet endpoint

--- stationspinner/character/serializers.py
+++ stationspinner/character/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
-from stationspinner.character.models import CharacterSheet
+from stationspinner.character.models import CharacterSheet, Skill, \
+    SkillInTraining, SkillQueue
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        exclude = ('owner',)
+
+class SkillQueueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SkillQueue
+        exclude = ('owner',)
+
+class SkillInTrainingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SkillInTraining
+        exclude = ('owner',)
 
 class CharacterSheetSerializer(serializers.HyperlinkedModelSerializer):
+    skills = SkillSerializer(many=True)
+    skillQueue = SkillQueueSerializer(many=True)
+    skillInTraining = SkillInTrainingSerializer()
+
     class Meta:
         model = CharacterSheet
-        exclude = ('owner', 'enabled')
+        exclude = ('owner', 'enabled', 'owner_key')
+
