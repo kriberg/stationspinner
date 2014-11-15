722bcafbacf1a86ebf8deb0270662f24ef1ec051
Kristian Berg <kriberg@tihlde.org>
2014-11-16 00:09:55 +0100

    Add some output help

--- stationspinner/accounting/management/commands/addapikey.py
+++ stationspinner/accounting/management/commands/addapikey.py
@@ -1,10 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
 from stationspinner.accounting.models import Capsuler, APIKey
+import sys
 class Command(BaseCommand):
-    args = '<capsuler name keyID vCode>'
+    args = '<capsuler> <key name> <keyID> <vCode>'
     help = 'Add an API key to a capsuler. For testing purposes only.'
 
     def handle(self, *args, **options):
+        if not len(args) == 4:
+            print self.help, self.args
+            sys.exit(1)
         capsuler = Capsuler.objects.get(username=args[0])
         key = APIKey(name=args[1],
                      keyID=args[2],
