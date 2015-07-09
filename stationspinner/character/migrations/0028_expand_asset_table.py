d03a3629d513c5eb95795f63b74d366ea624b015
Kristian Berg <kriberg@tihlde.org>
2015-07-09 20:57:08 +0000

    Add convenience fields to asset tables

---
+++ stationspinner/character/migrations/0028_expand_asset_table.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('character', '0027_auto_20150518_1938.py'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql = '''
+            ALTER TABLE character_asset ADD COLUMN "regionID" BIGINT;
+            ALTER TABLE character_asset ADD COLUMN "solarSystemID" BIGINT;
+            COMMIT;
+            '''
+        ),
+    ]
