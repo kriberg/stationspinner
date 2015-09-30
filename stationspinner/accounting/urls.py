304b39b9e0cc10a07b75b989cbdeab65854fbfc4
Kristian Berg <kriberg@tihlde.org>
2015-09-30 21:09:32 +0000

    Add endpoint to force revalidation of key

--- stationspinner/accounting/urls.py
+++ stationspinner/accounting/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from stationspinner.accounting.views import APIKeyViewset, \
-    CapsulerViewset, LogoutView, MissingTrainingViewset
+    CapsulerViewset, LogoutView, MissingTrainingViewset, RevalidateKeyView
 
 router = routers.DefaultRouter()
 router.register(r'capsuler', CapsulerViewset, 'capsuler')
@@ -9,6 +9,7 @@ router.register(r'missing-training', MissingTrainingViewset, 'missing-training')
 router.register(r'apikeys', APIKeyViewset, 'apikeys')
 
 urlpatterns = [
-    url(r'^logout/$', LogoutView.as_view()),
+    url(r'^logout/$', LogoutView.as_view(), name='accounting_logout'),
+    url(r'^revalidate-key/$', RevalidateKeyView.as_view(), name='accounting_revalidate_key'),
     url(r'^', include(router.urls)),
 ]
--- stationspinner/accounting/views.py
+++ stationspinner/accounting/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import View
+from django.shortcuts import get_object_or_404
 from django.contrib.auth import logout
 from django.http import HttpResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, views
 from rest_framework.response import Response
 from stationspinner.accounting.serializers import APIKeySerializer, \
     CapsulerSerializer
@@ -9,6 +10,7 @@ from stationspinner.accounting.models import APIKey, Capsuler
 from stationspinner.character.models import SkillInTraining
 from stationspinner.universe.models import APICall
 from stationspinner.libs.drf_extensions import CapsulerPermission, CapsulerViewset
+from stationspinner.celery import app
 
 
 class APIKeyViewset(CapsulerViewset):
@@ -18,6 +20,24 @@ class APIKeyViewset(CapsulerViewset):
 
     def get_queryset(self):
         return APIKey.objects.filter(owner=self.request.user)
+
+
+class RevalidateKeyView(views.APIView):
+    permission_classes = [CapsulerPermission]
+
+    def post(self, request):
+        id = request.data.get('id', None)
+        if id:
+            try:
+                key = APIKey.objects.get(pk=id, owner=request.user)
+            except APIKey.DoesNotExist:
+                return Response({'msg': 'No such APIKey.'}, status=404)
+            app.send_task('accounting.validate_key', [key.pk])
+            return Response({'msg': 'Revalidation queued.'})
+
+        return Response({'msg': 'No such APIKey'}, status=404)
+
+
 
 
 class CapsulerViewset(viewsets.ModelViewSet):
